35873fcda5ab58d81d50773a851fd7c7d32fe180
Khansa Kiasatina <kiasatina.k@gmail.com>
2020-03-14 14:36:41 -0400

    update updateProfile mutation

--- backend/src/graphql/mutations/updateProfile.js
+++ backend/src/graphql/mutations/updateProfile.js
@@ -2,30 +2,40 @@ const { withSession, check } = require('../../utils');
 const { User, Image } = require('../../models');
 
 //{ username, password, bio, profile_image }
-module.exports = withSession(async (root, { input }, ctx) => {
-    const { profile_image, ...args } = input;
-    const user = await User.findById(ctx.user, 'profile_image');
+module.exports = withSession(
+    async (root, { profile_image, username, password, bio }, ctx) => {
+        let args = {};
+        const user = await User.findById(ctx.user, 'profile_image');
 
-    if (args.username !== undefined) {
-        await check.username(args.username, true);
-    }
-
-    if (args.password !== undefined) {
-        await check.password(args.password);
-    }
-
-    if (profile_image) {
-        await check.file(profile_image, 'image');
-        if (user.profile_image) {
-            await Image.reupload(user.profile_image, profile_image);
-        } else {
-            const { _id } = await Image.upload(profile_image);
-            args.profile_image = _id;
+        if (username !== undefined) {
+            await check.username(username, true);
+            args.username = username;
         }
-    } else if (profile_image === null && user.profile_image) {
-        await Image.remove(user.profile_image);
-        args.profile_image = null;
-    }
 
-    return await User.findByIdAndUpdate({ _id: ctx.user }, args, { new: true });
-});
+        if (password !== undefined) {
+            await check.password(password);
+            args.password = username;
+        }
+
+        if (bio !== undefined) {
+            args.bio = bio;
+        }
+
+        if (profile_image) {
+            await check.file(profile_image, 'image');
+            if (user.profile_image) {
+                await Image.reupload(user.profile_image, profile_image);
+            } else {
+                const { _id } = await Image.upload(profile_image);
+                args.profile_image = _id;
+            }
+        } else if (profile_image === null && user.profile_image) {
+            await Image.remove(user.profile_image);
+            args.profile_image = null;
+        }
+
+        return await User.findByIdAndUpdate({ _id: ctx.user }, args, {
+            new: true,
+        });
+    },
+);
